e341f3ca3303da120ae292e67da9eef9b0a97f26
neathcmd <ksdf71464@gmail.com>
2025-05-22 15:41:14 +0700

    add new collection type

--- src/api/single-blog/controllers/single-blog.ts
+++
@@ -1,7 +0,0 @@
-/**
- * single-blog controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::single-blog.single-blog');
--- src/api/single-blog/routes/single-blog.ts
+++
@@ -1,7 +0,0 @@
-/**
- * single-blog router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::single-blog.single-blog');
